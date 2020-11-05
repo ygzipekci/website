bb1ad7318a21aa05259efff56d18661a5004bdc6
ygzipekci <73952396+ygzipekci@users.noreply.github.com>
2020-11-05 23:33:46 +0300

    integrated camera and controls
    made it interact with the camera, made the rotation controllable and changed the color.

--- CodingChallenges/CC_011_PerlinNoiseTerrain/P5/sketch.js
+++ CodingChallenges/CC_011_PerlinNoiseTerrain/P5/sketch.js
@@ -6,18 +6,26 @@
 // Edited by SacrificeProductions
 
 var cols, rows;
-var scl = 20;
-var w = 1400;
-var h = 1000;
-
-var flying = 0;
+var scl = 15;
+var w;
+var h;
 
 var terrain = [];
 
+var video;
+
+var shift = 0;
+var low;
+var high;
+
 function setup() {
-  createCanvas(600, 600, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  w = width;
+  h = height;
   cols = w / scl;
   rows = h / scl;
+
+  video = createCapture(VIDEO);
 
   for (var x = 0; x < cols; x++) {
     terrain[x] = [];
@@ -28,25 +36,39 @@ function setup() {
 }
 
 function draw() {
-  flying -= 0.1;
-  var yoff = flying;
-  for (var y = 0; y < rows; y++) {
-    var xoff = 0;
-    for (var x = 0; x < cols; x++) {
-      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
-      xoff += 0.2;
-    }
-    yoff += 0.2;
-  }
-
+  video.size(w / scl, h / scl);
   background(0);
+
+  video.loadPixels();
+  loadPixels();
+  shift -= 0.03;
+  var yOffset = shift;
+  //var yOffset = 0;
+  for (var y = 0; y < video.height; y++) {
+    var xOffset = 0;
+    for (var x = 0; x < video.width; x++) {
+      var index = (video.width - x - 1 + y * video.width) * 4;
+      var r = video.pixels[index + 0];
+      var g = video.pixels[index + 1];
+      var b = video.pixels[index + 2];
+      var brightness = (r + b + g) / 3;
+      
+      terrain[x][y] = map(brightness, 0, 255, -50, 100) + map(noise(xOffset, yOffset), 0, 1, -20, 20);
+      xOffset += 0.1;      
+    }
+    yOffset += 0.1;
+  }
+ 
+
+  background(0, 29, 95);
   translate(0, 50);
-  rotateX(PI / 3);
-  fill(200, 200, 200, 50);
+  rotateX(map(mouseY, 0, height, PI/2, -PI/8));
+  noFill();
+  stroke(255, 0, 253);
   translate(-w / 2, -h / 2);
-  for (var y = 0; y < rows - 1; y++) {
+  for (y = 0; y < rows -2; y++) {
     beginShape(TRIANGLE_STRIP);
-    for (var x = 0; x < cols; x++) {
+    for (x = 0; x < cols -1; x++) {
       vertex(x * scl, y * scl, terrain[x][y]);
       vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
     }
